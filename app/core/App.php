405ed054b0f4dfd7e71e17f5348c852b9133e466
Sansão Maposse <121869087+SansaoMaposse@users.noreply.github.com>
2024-02-11 14:37:11 +0200

    ola mundo

--- app/core/App.php
+++ app/core/App.php
@@ -1,5 +1,18 @@
 <?php
 
+class App{
+    protected $controller = "HomeController";
+    protected $action = "index";
+    protected $params = [];
+
+    public function __construct(){
+        $url = $_SERVER['QUERY_STRING'];
+
+        $url = explode("/", $url);
+
+        echo $url[0];
+    }   
+}
 
 
 ?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
-    echo 'ola mundo';
+    define("ROOT_PATH", dirname(__DIR__));
+    require_once ROOT_PATH . '/app/core/App.php';
+    // echo $_SERVER['QUERY_STRING'];
+
+    new App();
 
 ?>
